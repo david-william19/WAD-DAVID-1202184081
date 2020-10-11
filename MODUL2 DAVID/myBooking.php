f758e8015360c2ef1bf48ad93fed88a52be0aa16
david-william <55936252+david-william19@users.noreply.github.com>
2020-10-11 21:41:19 +0800

    MODUL 2

--- MODUL2 DAVID/myBooking.php
+++ MODUL2 DAVID/myBooking.php
@@ -11,6 +11,7 @@
 
 <body>
      <?php
+     error_reporting(error_reporting() & ~E_NOTICE);
           $idNumber = rand(2340, 3000);
           $name = $_POST['nama'];
           $checkIn = $_POST['checkin'];
@@ -18,16 +19,15 @@
           $checkOut = $checkIn." + ".$duration." days";
           $room = $_POST['room'];
           $phone = $_POST['nomor'];
-          $servicePrice = count($_POST['service']);
 
           if($room == "https://www.jalajahnusae.com/wp-content/uploads/2019/08/teraskita-hotel.jpg"):
                $room = 'Standard';
           elseif ($room == "https://avenzelhotel.com/wp-content/uploads/2016/03/suite-bedroom-king.jpg"):
                $room = 'Superior';
-               $harga = 120;
+               $harga = 150;
           elseif ($room == "https://disk.mediaindonesia.com/thumbs/1800x1200/news/2019/07/cd2515e1ba09506196b36afa004d449f.jpg"):
                $room = 'Luxury';
-               $harga = 150;
+               $harga = 200;
           endif;
           
           if($room == 'Standard'):
@@ -38,7 +38,7 @@
                $harga = '200' * $duration;
           endif;
      ?>
-          
+
      <nav class="navbar navbar-expand-lg navbar-light bg-danger">
           <div class="collapse navbar-collapse d-flex justify-content-center">
                <div class="navbar-nav">
@@ -48,43 +48,51 @@
           </div>
      </nav>
 
-          <div class="title mt-2 text-center">
-               <h2 class="text-danger">EAD HOTEL</h2>
-               <p class="text-danger">Welcome to 5 Star Hotel</p>
-          </div>
+     <div class="title mt-2 text-center">
+          <h2 class="text-danger">EAD HOTEL</h2>
+          <p class="text-danger">Welcome to 5 Star Hotel</p>
+     </div>
 
-          <div class="container d-flex justify-content-center p-2">
-               <table class="table table-striped">
-                    <tr>
-                         <th>Booking Number</th>
-                         <th>Name</th>
-                         <th>Check-in</th>
-                         <th>check-out</th>
-                         <th>Room Type</th>
-                         <th>Phone Number</th>
-                         <th>Service</th>
-                         <th>Total Price</th>
-                    </tr>
-                    <tr>
-                         <td><?= $idNumber?></td>
-                         <td><?= $name?></td>
-                         <td><?= date('d-m-Y', strtotime($checkIn))?></td>
-                         <td><?= date('d-m-Y', strtotime($checkOut))?></td>
-                         <td><?= $room;?></td>
-                         <td><?= $phone?></td>
-                         <td><?php if(!empty($_POST['service'])) {
+     <div class="container d-flex justify-content-center p-2">
+          <table class="table table-striped">
+               <tr>
+                    <th>Booking Number</th>
+                    <th>Name</th>
+                    <th>Check-in</th>
+                    <th>check-out</th>
+                    <th>Room Type</th>
+                    <th>Phone Number</th>
+                    <th>Service</th>
+                    <th>Total Price</th>
+               </tr>
+               <tr>
+                    <td><?= $idNumber?></td>
+                    <td><?= $name?></td>
+                    <td><?= date('d-m-Y', strtotime($checkIn))?></td>
+                    <td><?= date('d-m-Y', strtotime($checkOut))?></td>
+                    <td><?= $room;?></td>
+                    <td><?= $phone?></td>
+                    <td>
+                         <?php if(!empty($_POST['service'])) {
            
                               foreach($_POST['service'] as $serv){
                                   echo $serv.'<br/>';
-                              }?>
-                         </td>
-                         <td><?= $harga+$servicePrice*20?></td>
-                    <?php } else {?>
-                         <td><?=$harga?></td>
-                    <?php }?>
-                    </tr>
-               </table>
-          </div>
+                              }
+                              }elseif(empty($_POST['service'])) {
+                                   echo 'no service';}?>
+
+
+                    </td>
+                    <td>
+                    <?php if(!empty($_POST['service'])){
+                         echo $hargaTotal = $duration*$harga+($duration*(count($_POST['service'])*20));
+                    } else {
+                         echo $harga;
+                    }?>
+                    </td>
+               </tr>
+          </table>
+     </div>
 </body>
 
 </html>
